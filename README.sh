5311e20cce0bc0cbdfd574056da82d0538352492
Mateusz Chojnacki <mateuszchojnacki@Mateuszs-MacBook-Pro.local>
2018-11-12 21:11:54 +0100

    done script

---
+++ README.sh
@@ -0,0 +1,7 @@
+## FindPhoto.sh
+
+Typical situation me and my girlfriend came back from trip. And we have something like 8k photos. And she told me send me photos with number 1846,1847, 1851,1861,1874, ,1881, 1885, 1886 ... and more. 
+
+God damn i'm programer i don't wanna look for these photos 'by hand' And this situation always repeats itself... 
+
+That's why this script.
